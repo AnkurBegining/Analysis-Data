76f98a1f5ed5cfe35bcb604d000f20e7916a6da0
AnkurBegining <201551016.iiitvadodara.ac.in@gmail.com>
2017-06-12 15:17:18 +0545

    1.2: feat-Added function parse_date to convert date string into python datetime date type

--- Ankur/workingWithCsvFile.py
+++ Ankur/workingWithCsvFile.py
@@ -1,3 +1,5 @@
+from datetime import datetime as dt
+
 from unicodecsv import *
 
 
@@ -17,3 +19,14 @@ submissions = openCsvFile('project_submissions.csv')
 print(enrollments[0])
 print(daily_engagement[0])
 print(submissions[0])
+
+
+# clean of code by reformatting data type
+
+
+# for parsing date into another data type
+def parse_date(date):
+    if date == '':
+        return None
+    else:
+        return dt.strptime(date, '%Y-%m-%d')
